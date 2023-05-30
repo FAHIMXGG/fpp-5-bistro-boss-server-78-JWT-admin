2aaef56ed2ef69142a5fec47048a9307323d91c2
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-30 16:00:57 +0600

    admin

--- index.js
+++ index.js
@@ -31,11 +31,37 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
     // user
+    app.get('/users',async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const result = await usersCollection.insertOne(user)
-      res.send(result)
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      //console.log('ex', existingUser)
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     });
+
+    app.patch('/users/admin/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set : {
+          role: "admin"
+        }
+      }
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
 
     //menu
